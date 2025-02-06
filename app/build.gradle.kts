7d38a48c49a2a9bab89ae00fa2b58c7e4b9a22b4
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-02-05 22:49:53 -0300

    chore: update target sdk to Android 15 (API level 35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,9 +13,9 @@ android {
 	compileSdk = 35
 
 	defaultConfig {
-		applicationId = "com.meninocoiso.beatstarcommunity"
+		applicationId = "com.meninocoiso.bscm"
 		minSdk = 24
-		targetSdk = 34
+		targetSdk = 35
 		versionCode = 1
 		versionName = "1.0"
 
@@ -53,6 +53,7 @@ android {
 			excludes += "/META-INF/{AL2.0,LGPL2.1}"
 		}
 	}
+	buildToolsVersion = "35.0.0"
 }
 
 dependencies {
