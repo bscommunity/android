bdd94edd0b900a889cce92367c1f0c29f3b83311
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-02-22 23:46:22 -0300

    refactor: subtle file system reorganize and icons disposing on chart details page

---
+++ app/src/main/java/com/meninocoiso/beatstarcommunity/service/CacheService.kt
@@ -0,0 +1,11 @@
+package com.meninocoiso.beatstarcommunity.service
+
+import com.meninocoiso.beatstarcommunity.domain.model.Chart
+
+class CacheService {
+	companion object {
+		fun cacheCharts(chart: List<Chart>) {
+			// Cache chart
+		}
+	}
+}
