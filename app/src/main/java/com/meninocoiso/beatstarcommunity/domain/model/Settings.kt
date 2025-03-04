900406ff9d62f615151ac2bbab3b844eade3665f
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-03-04 02:41:57 -0300

    fixup! refactor: changed UserPreferences naming to Settings

--- app/src/main/java/com/meninocoiso/beatstarcommunity/domain/model/Settings.kt
+++ app/src/main/java/com/meninocoiso/beatstarcommunity/domain/model/Settings.kt
@@ -2,7 +2,7 @@ package com.meninocoiso.beatstarcommunity.domain.model
 
 import com.meninocoiso.beatstarcommunity.domain.enums.ThemePreference
 
-data class UserPreferences(
+data class Settings(
 	val allowExplicitContent: Boolean,
 	val useDynamicColors: Boolean,
 	val theme: ThemePreference
