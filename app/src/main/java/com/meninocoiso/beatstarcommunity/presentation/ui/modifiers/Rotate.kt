1c9726620ee354122399847b390401ea1323b270
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-03-06 20:28:42 -0300

    feat: better user experience on updates page

---
+++ app/src/main/java/com/meninocoiso/beatstarcommunity/presentation/ui/modifiers/Rotate.kt
@@ -0,0 +1,37 @@
+package com.meninocoiso.beatstarcommunity.presentation.ui.modifiers
+
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+
+@Composable
+fun Modifier.infiniteRotation(
+    durationMillis: Int = 1000,
+    easing: Easing = LinearEasing,
+): Modifier {
+    val transition = rememberInfiniteTransition(label = "")
+
+    val translateAnimation by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = durationMillis,
+                easing = easing,
+            ),
+            repeatMode = RepeatMode.Restart,
+        ),
+    )
+
+    return graphicsLayer {
+        rotationZ = translateAnimation
+    }
+}
