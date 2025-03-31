6e2080a38775593b62d844e085a6ead7c3305281
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-03-30 23:06:48 -0300

    ci: fix apk signing

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ To start the chart deep link:
 ```bash
 ./adb shell am start -a android.intent.action.VIEW -d "bscm://chart/details/8d3d2eb4-5218-48af-ae33-a73413889353" com.meninocoiso.bscm
 ```
+
+To convert `keystore.jks` to `keystore.b64`:
+```bash
+base64 -w 0 app/keystore.jks > keystore.b64
+```
--- app/src/main/java/com/meninocoiso/beatstarcommunity/presentation/ui/components/ExposedDropdownMenuBoxUI.kt
+++ app/src/main/java/com/meninocoiso/beatstarcommunity/presentation/ui/components/ExposedDropdownMenuBoxUI.kt
@@ -3,10 +3,10 @@ package com.meninocoiso.beatstarcommunity.presentation.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -36,7 +36,7 @@ fun ExposedDropdownMenuBoxUI(
 			// expanding/collapsing the menu on click. A read-only text field has
 			// the anchor type `PrimaryNotEditable`.
 			modifier = Modifier
-				.menuAnchor(MenuAnchorType.PrimaryNotEditable)
+				.menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)
 				.fillMaxWidth(),
 			value = currentOption,
 			onValueChange = {},
