07eb83e0ec4e9b3866d856c7bf8d10fd3474e4cf
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-05-09 14:24:47 -0300

    fix: UpdatesSection serialization

--- app/src/main/java/com/meninocoiso/beatstarcommunity/domain/enums/UpdatesSection.kt
+++ app/src/main/java/com/meninocoiso/beatstarcommunity/domain/enums/UpdatesSection.kt
@@ -1,9 +1,13 @@
 package com.meninocoiso.beatstarcommunity.domain.enums
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@SerialName("com.meninocoiso.beatstarcommunity.domain.enums.UpdatesSection")
 enum class UpdatesSection {
+    @SerialName("Workshop")
     Workshop,
+    @SerialName("Installations")
     Installations
 }
