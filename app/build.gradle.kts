27f96370f066ecea122e9c819af7f55c2c969917
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-06-02 01:01:06 -0300

    ci: bumped version to 5 (0.0.5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
 		// to use the new Date API, since desugar is not working
 		minSdk = 26
 		targetSdk = 35
-		versionCode = 4
-		versionName = "0.0.4-beta"
+		versionCode = 5
+		versionName = "0.0.5-beta"
 
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 		vectorDrawables {
