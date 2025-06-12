4418a21468cd6b8b9b6073af35407ad8408b5c6f
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-06-12 01:18:48 -0300

    fix: deep links from not cached charts should now work normally

--- app/src/main/java/com/meninocoiso/bscm/presentation/screens/details/ChartDetailsRoute.kt
+++ app/src/main/java/com/meninocoiso/bscm/presentation/screens/details/ChartDetailsRoute.kt
@@ -1,11 +1,11 @@
 package com.meninocoiso.bscm.presentation.screens.details
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,7 +22,7 @@ import com.meninocoiso.bscm.presentation.viewmodel.DetailsState
 @Composable
 fun ChartDetailsRoute(
     chartId: String?,
-    onReturn : () -> Unit,
+    onReturn: () -> Unit,
     viewModel: ChartDetailsViewModel = hiltViewModel()
 ) {
     val chartState by viewModel.chart.collectAsStateWithLifecycle()
@@ -31,45 +31,45 @@ fun ChartDetailsRoute(
     LaunchedEffect(chartId) {
         viewModel.fetchChartById(chartId)
     }
+    
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = MaterialTheme.colorScheme.background,
+        contentColor = MaterialTheme.colorScheme.onBackground
+    ) { it
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            when (chartState) {
+                is DetailsState.Loading -> {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(24.dp),
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
 
-    when (chartState) {
-        is DetailsState.Loading -> {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.background),
-                contentAlignment = Alignment.Center
-            ) {
-                CircularProgressIndicator(
-                    modifier = Modifier.size(24.dp),
-                    color = MaterialTheme.colorScheme.primary
-                )
-            }
-        }
-        is DetailsState.Success -> {
-            ChartDetailsScreen(
-                chart = (chartState as DetailsState.Success).chart,
-                onReturn = onReturn
-            )
-        }
-        is DetailsState.Error -> {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colorScheme.background)
-                ,
-                contentAlignment = Alignment.Center
-            ) {
-                StatusMessageUI(
-                    title = "Failed to load chart details",
-                    icon = R.drawable.rounded_error_24,
-                    message = (chartState as DetailsState.Error).message
-                        ?: "An error occurred while loading the chart details",
-                    buttonLabel = "Retry",
-                    onClick = {
-                        viewModel.fetchChartById(chartId)
-                    }
-                )
+                is DetailsState.Success -> {
+                    ChartDetailsScreen(
+                        chart = (chartState as DetailsState.Success).chart,
+                        onReturn = onReturn
+                    )
+                }
+
+                is DetailsState.Error -> {
+                    StatusMessageUI(
+                        title = "Failed to load chart details",
+                        icon = R.drawable.rounded_error_24,
+                        message = (chartState as DetailsState.Error).message
+                            ?: "An error occurred while loading the chart details",
+                        buttonLabel = "Retry",
+                        onClick = {
+                            viewModel.fetchChartById(chartId)
+                        },
+                    )
+
+                }
             }
         }
     }
