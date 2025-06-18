bc7f667d1ec02d8c58a1da4d0a5a4958ac52dd16
Eduardo Maciel <eduardomacielbr@gmail.com>
2025-06-18 19:03:09 -0300

    feat: added contributors names to the language picker dialog

--- app/src/main/java/com/meninocoiso/bscm/presentation/ui/components/dialog/LanguageDialog.kt
+++ app/src/main/java/com/meninocoiso/bscm/presentation/ui/components/dialog/LanguageDialog.kt
@@ -1,24 +1,43 @@
 package com.meninocoiso.bscm.presentation.ui.components.dialog
 
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.core.os.LocaleListCompat
 import com.meninocoiso.bscm.R
 import com.meninocoiso.bscm.presentation.ui.components.RadioGroupUI
+import com.meninocoiso.bscm.util.LinkingUtils
 
 data class SupportedLanguage(val tag: String?, val displayName: String)
 
+private const val CONTRIBUTORS_LINK =
+    "https://docs.google.com/spreadsheets/d/1pPn-XXC_2ivgPXWlPcgxU2jun8Kq9u3nKlpBFSoljmM/edit?usp=sharing"
+
 @Composable
 fun LanguageDialog() {
+    val context = LocalContext.current
     val (isOpened, setIsOpened) = remember { mutableStateOf(false) }
 
     val systemDefault = SupportedLanguage(null, stringResource(R.string.system_default_language))
@@ -26,8 +45,8 @@ fun LanguageDialog() {
         systemDefault,
         SupportedLanguage("en-US", "English"),
         SupportedLanguage("pt-BR", "Português (Brasil)"),
-        SupportedLanguage("es-ES", "Español"),
-        SupportedLanguage("ru-RU", "Русский")
+        SupportedLanguage("es-ES", "Español (by Farfu)"),
+        SupportedLanguage("ru-RU", "Русский (by MusicCat)")
     )
 
     val currentLocaleTag = AppCompatDelegate.getApplicationLocales()[0]?.toLanguageTag()
@@ -51,19 +70,48 @@ fun LanguageDialog() {
                 )
             },
             text = {
-                RadioGroupUI(
-                    initialSelected = selectedLanguage.value.displayName,
-                    radioOptions = supportedLanguages.map { it.displayName },
-                    onOptionSelected = { index, _ ->
-                        val lang = supportedLanguages[index]
-                        if (lang.tag == null) {
-                            AppCompatDelegate.setApplicationLocales(LocaleListCompat.getEmptyLocaleList())
-                        } else {
-                            AppCompatDelegate.setApplicationLocales(LocaleListCompat.forLanguageTags(lang.tag))
-                        }
-                        selectedLanguage.value = lang
-                    }
-                )
+                Column {
+                    RadioGroupUI(
+                        initialSelected = selectedLanguage.value.displayName,
+                        radioOptions = supportedLanguages.map { it.displayName },
+                        onOptionSelected = { index, _ ->
+                            val lang = supportedLanguages[index]
+                            if (lang.tag == null) {
+                                AppCompatDelegate.setApplicationLocales(LocaleListCompat.getEmptyLocaleList())
+                            } else {
+                                AppCompatDelegate.setApplicationLocales(
+                                    LocaleListCompat.forLanguageTags(
+                                        lang.tag
+                                    )
+                                )
+                            }
+                            selectedLanguage.value = lang
+                        },
+                    )
+                    /*Box(modifier = Modifier.padding(8.dp), contentAlignment = Alignment.BottomEnd) {
+                        Text(text = stringResource(R.string.language_dialog_info))
+                        Icon(
+                            painter = painterResource(R.drawable.open_in_new_24px),
+                            modifier = Modifier.size(12.dp),
+                            contentDescription = null,
+                        )
+                    }*/
+                    Text(
+                        text = buildAnnotatedString {
+                            append(stringResource(R.string.language_dialog_info))
+                            appendInlineContent(inlineContentId, "[icon]")
+                        },
+                        modifier = Modifier
+                            .padding(top = 12.dp)
+                            .clickable(onClick = {
+                                LinkingUtils.openLink(context, CONTRIBUTORS_LINK)
+                            }),
+                        style = MaterialTheme.typography.labelLarge,
+                        color = MaterialTheme.colorScheme.primary,
+                        lineHeight = 20.sp,
+                        inlineContent = inlineContent
+                    )
+                }
             },
             dismissButton = {
                 TextButton(onClick = { setIsOpened(false) }) {
@@ -78,3 +126,24 @@ fun LanguageDialog() {
         )
     }
 }
+
+val inlineContentId = "open_in_new_icon"
+
+val inlineContent = mapOf(
+    inlineContentId to InlineTextContent(
+        Placeholder(
+            width = 18.sp,
+            height = 12.sp,
+            placeholderVerticalAlign = PlaceholderVerticalAlign.Center
+        )
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.open_in_new_24px),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(start = 6.dp)
+        )
+    }
+)
