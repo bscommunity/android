8b7489e0f1dae0806feec288f2dc48aa6f7501a1
meninocoiso <225307799+meninocoiso@users.noreply.github.com>
2025-09-07 04:58:52 -0300

    ci: downgraded CMake to 3.22.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -95,7 +95,7 @@ android {
     externalNativeBuild {
         cmake {
             path = file("src/main/cpp/CMakeLists.txt")
-            version = "4.1.0"
+            version = "3.22.1"
         }
     }
 }
